a06a849daaba49ab08d61c6df2f27c6df46fd8b6
TURKCELL\EXT02D54066 <furkan.abbasioglu@consultant.turkcell.com.tr>
2024-09-01 16:53:56 +0300

    Initial Commit

---
+++ wassup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wassup
+//
+//  Created by Furkan Abbasioğlu on 1.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
